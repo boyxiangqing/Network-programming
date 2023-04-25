04c11e32d5a70da509ab70dfccc940fdd604bd4d
xiangqingkou@163.com <xianqingkou@kengic.com>
2023-04-25 17:34:25 +0800

    4.25 afternoon

--- src/com/carl/upload/TCPFileUploadServer.java
+++ src/com/carl/upload/TCPFileUploadServer.java
@@ -1,45 +1,31 @@
 package com.carl.upload;
 
-import com.carl.util.StreamUtils;
+import com.carl.utils.StreamUtils;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
  * @author Carl
  * @version 1.0
+ * 文件上传的服务端
  */
 public class TCPFileUploadServer {
     public static void main(String[] args) throws Exception {
-        //服务端监听8888端口
+
+        //1.服务端监听8888端口
         ServerSocket serverSocket = new ServerSocket(8888);
         System.out.println("服务端在8888开启监听...");
         //2.等待连接
         Socket socket = serverSocket.accept();
 
         //3.读取客户端发送的数据
-        // 通过socket得到一个输入流
+        // 通过Socket得到输入流
         BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
         byte[] bytes = StreamUtils.streamToByteArray(bis);
-        //4.将得到bytes数组,写入到指定的路径,就得到一个文件
-        String destFilePath = "src/qier2.png";
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(destFilePath));
-        bos.write(bytes);
 
 
-        //向客户端发送"收到图片"
-        //通过socket获取到输出流(字符)
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        writer.write("收到图片");
-        writer.flush();//把内容刷新到我们的数据
-        socket.shutdownOutput();//设置写入结束标记
-
-        //关闭其他资源
-        bos.close();
-        writer.close();
-        bis.close();
-        socket.close();
-        serverSocket.close();
     }
 }
